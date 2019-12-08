a6cf918260a3930e21f2ef866ae0e3f8ec9b089d
Tom Forbes <tom@tomforb.es>
2019-12-08 22:27:21 +0000

    No tests for now!

--- src/main.rs
+++ src/main.rs
@@ -98,15 +98,15 @@ mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
-    #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
-    }
-
-    #[test]
-    fn test_bad_add() {
-        // This assert would fire and test will fail.
-        // Please note, that private functions can be tested too!
-        assert_eq!(bad_add(1, 2), 3);
-    }
+//    #[test]
+//    fn test_add() {
+//        assert_eq!(add(1, 2), 3);
+//    }
+//
+//    #[test]
+//    fn test_bad_add() {
+//        // This assert would fire and test will fail.
+//        // Please note, that private functions can be tested too!
+//        assert_eq!(bad_add(1, 2), 3);
+//    }
 }
